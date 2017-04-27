51e97a80503952b9726749a3b3d5a712829784cb
Vinícius Lima <vinicius.lima.cordeiro@usp.br>
2017-04-27 08:24:47 -0300

    Update prob_spike.m
    Small changes in bar graphs.

--- prob_spike.m
+++ prob_spike.m
@@ -5,8 +5,8 @@ clc
 
 tic
 
-data_matrix = load('m_pot.dat');
-
+data_matrix = load('190_60_47_13.dat');
+%data_matrix = data_matrix.m_;
 % Number of row and columns in data matrix
 [r, c] = size(data_matrix);
 
@@ -31,7 +31,7 @@ thr_values = [];
 % values below the found threhold for a particular expriment
 data_matrix2 = [];
 
-t = 0:0.05:5;
+t = 0:0.05:10;
 
 % figure(1);
 % subplot(1,2,1);
@@ -55,13 +55,13 @@ for i = 1:Npeaks-1
     aux( aux > thr_values(i) ) = NaN;
     data_matrix2 = [data_matrix2 ; aux];
     %Plot the separated peaks, with the found threshold
-%     if mod(count, 1000) == 0
+%     if mod(count, 10000) == 0
 %         figure(1);
-%         plot(t,data_matrix(index(i)-60:index(i)+40),'black');
+%         plot(t,data_matrix(index(i)-60:index(i)+140),'black');
 %         hold on;
 %         plot(t(60-60+aux2), P(aux2),'Ored');
 %     end
-%     count = count + 1;
+%      count = count + 1;
 end
 
 % ylabel('Membrane Potencial [mV]')
@@ -71,26 +71,37 @@ end
 % print('potencial_thr_values','-dpng','-r600')
 
 %%
-bl = .2;            % Length of the bins
+bl = 0.5;            % Length of the bins
 
 % Dividing into bins
-v_m = -80:bl:1;
+v_m = -76:bl:1;
 [n1, x1] = hist(data_matrix2,v_m);
 [n2, x2] = hist(thr_values,v_m);
 phi_v = n2 ./ n1;
 figure
-plot(v_m,phi_v);
-axis([-50 -44.6 0 1.1])
+% plot(v_m, phi_v)
+plot(v_m(~isnan(phi_v))-10,phi_v(~isnan(phi_v))); %cesar
+% axis([-50 -44.6 0 1.1])  % simul
+% axis([-60 -40 0 1]) %cesar
 ylabel('Probability')
 xlabel('Membrane Potencial [mV]')
 % print('graf_prob_disp','-dpng','-r600')
 
 %%
 figure
-plotyy(x1,n1,x2,n2)
-ylabel('Counts')
-xlabel('Membrane Potential [mV]');
-legend('V_{m}', 'V_{th}');
+[ax, h1, h2] = plotyy(x1,n1,x2,n2,@stairs);
+% set([h1,h2],'plottype', 'stairs')
+% hold(ax)
+% h1 = stairs(x1,n1,'Color',[0 0 1]);
+% ax(1) = gca;
+% axpos = get(ax(1),'position');
+% axes('position',axpos);
+% h2 = stairs(x2,n2,'color',[0 1 0]);
+% ax(2) = gca;
+% set(ax(2),'Box','off','Visible','off');
+% ylabel('Counts')
+% xlabel('Membrane Potential [mV]');
+legend([h1, h2],'V_{m}', 'V_{th}');
 
 % print('hist_prob_disp','-dpng','-r600')
 
